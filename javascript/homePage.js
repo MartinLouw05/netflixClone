2044023718f75fbf7af0bdcb0090c69d84536696
Martin Louw <louw.martin05@gmail.com>
2022-07-29 14:35:06 +0200

    Added Movie Class
    Added a class for movies with relevant getters, setters and other functions.
    Other functions only includes the "createMovie" function.
    Adjusted the "createMovies" method to be inline with the newly created class.
    Added a method that populates the grid with movies and their information.
    Added a global click event listener that will identify if a user selects to add a movie to their watchlist.
    Added some CSS styling.
    Added the netflix logo image file as the url link was giving errors.

--- javascript/homePage.js
+++ javascript/homePage.js
@@ -14,12 +14,13 @@ else {
 //Random Movies Information
 let moviesInfo = JSON.parse(localStorage.getItem("moviesData"));
 let moviesBannersInfo = JSON.parse(localStorage.getItem("movieBannersData"));
+let moviesInstanceInfo;
 
 //On Home Page Load
 let pageStartup = new Vue ({
     el : "#pageHeader",
     data : {
-        logo : "https://www.freepnglogos.com/uploads/red-netflix-logo-text-png-3.png",
+        logo : "../images/netflixLogo.png",
         currentUser : localStorage.getItem("currentUser")
     },
     methods : {
@@ -43,6 +44,7 @@ let banners = new Vue ({
     methods : {
         createBanners : function() {
             let length = this.bannerImage.length;
+
             for (i = 0; i < length; i++) {
                 const newLine = document.createElement('div');
                 newLine.id = i;
@@ -69,10 +71,118 @@ let banners = new Vue ({
     }
 })
 
+//Movie Class
+class Movies {
+    constructor(id, name, genre, comingSoon, availDate, thumbnail, preview) {
+        this._id = id;
+        this._name = name;
+        this._genre = genre;
+        this._comingSoon = comingSoon;
+        this._availDate = availDate;
+        this._thumbnail = thumbnail;
+        this._preview = preview;
+    }
+
+    get id() {
+        return this._id;
+    }
+    set id(text) {
+        if (isNaN(text)) {
+            console.log("You Must Assign a Value To The ID");
+        } 
+        else {
+            this._id = text;
+        }
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(text) {
+        if (typeof text == "string") {
+            this._name = text;
+        } 
+        else {
+            console.log("You Must Assign a Value To The Name");
+        }
+    }
+
+    get genre() {
+        return this._genre;
+    }
+    set genre(text) {
+        if (typeof text == "string") {
+            this._genre = text;
+        } 
+        else {
+            console.log("You Must Assign a Value To The Genre");
+        }
+    }
+
+    get comingSoon() {
+        return this._comingSoon;
+    }
+    set comingSoon(text) {
+        if (typeof text == "boolean") {
+            this._comingSoon = text;
+        } 
+        else {
+            console.log("You Must Assign a Value To Coming Soon");
+        }
+    }
+
+    get availDate() {
+        return this._availDate;
+    }
+    set availDate(text) {
+        if (typeof text == "string") {
+            this._availDate = text;
+        } 
+        else {
+            console.log("You Must Assign a Value To The Available Date");
+        }
+    }
+
+    get thumbnail() {
+        return this._thumbnail;
+    }
+    set thumbnail(text) {
+        if (typeof text == "string") {
+            this._thumbnail = text;
+        } 
+        else {
+            console.log("You Must Assign a Value To The Thumbnail");
+        }
+    }
+
+    get preview() {
+        return this._preview;
+    }
+    set preview(text) {
+        if (typeof text == "string") {
+            this._preview = text;
+        } 
+        else {
+            console.log("You Must Assign a Value To The Preview");
+        }
+    }
+
+    //Create Movie Class Array
+    createMovie(data) {
+        let moviesArray = [];
+        moviesArray.push(data);
+
+        return moviesArray;
+    }
+}
+
 //Create Movie Instances
 let movies = new Vue ({
-    el : "",
+    el : "#moviesGrid",
+    elBtn : "button[class^='movieArea'",
     data : {
+        moviesData : JSON.parse(localStorage.getItem("moviesData")),
+        moviesList : [],
         id : "",
         name : "",
         genre : "",
@@ -83,14 +193,20 @@ let movies = new Vue ({
     },
     methods : {
         createMovies : function() {
+            let movieGridData = new Movies();
+
             for (i = 0; i < moviesInfo.length; i++) {
-                this.id = i;
-                this.name = moviesInfo[i].name;
-                this.genre = moviesInfo[i].genre;
-                this.comingSoon = this.computeComingSoon(i);
-                this.availDate = moviesInfo[i].availDate;
-                this.thumbnail = "<img src=\"" + moviesInfo[i].thumbnail + "\">";
-                this.preview = moviesInfo[i].preview;
+                movieGridData.id = i;
+                movieGridData.name = this.moviesData[i].name;
+                movieGridData.genre = this.moviesData[i].genre;
+                movieGridData.comingSoon = this.computeComingSoon(i);
+                movieGridData.availDate = this.moviesData[i].availDate;
+                movieGridData.thumbnail = this.moviesData[i].thumbnail;
+                movieGridData.preview = this.moviesData[i].preview;
+
+                let movieObj = { name : movieGridData.name, genre : movieGridData.genre, comingSoon : movieGridData.comingSoon, availDate : movieGridData.availDate, thumbnail : movieGridData.thumbnail, preview : movieGridData.preview};
+                this.moviesInstance = movieGridData.createMovie(movieObj);
+                this.moviesList.push(movieObj);                
             }
         },
         computeComingSoon : function(index) {
@@ -100,19 +216,92 @@ let movies = new Vue ({
             let yyyy = today.getFullYear();
             today = yyyy + '-' + mm + '-' + dd;
 
-            if (today > moviesInfo[index].availDate) {
+            if (today > this.moviesData[index].availDate) {
                 return true;
             } 
             else {
                 return false;
             }
+        },
+        createMoviesGrid : function() {
+            let length = this.moviesList.length;
+
+            for (i = 0; i < length; i++) {
+                const newGrid = document.createElement('div');
+                newGrid.id = i;
+                newGrid.className = "movieArea";
+
+                let movieName = [];
+                movieName.push(this.moviesList[i].name);
+                let movieImg = [];
+                movieImg.push(this.moviesList[i].thumbnail);
+                let comingSoon = [];
+                comingSoon.push(this.moviesList[i].comingSoon);
+
+                for (x = 0; x < movieImg.length; x++) {
+                    const newName = document.createElement('h6');
+                    newName.innerHTML = movieName[x];
+                    const newDiv = document.createElement('div');
+                    newDiv.className = "movieImg";
+                    const newImg = document.createElement('img');
+                    newImg.className = "movieThumbnail";
+                    newImg.src = movieImg[x];
+
+                    newGrid.append(newName);
+                    newDiv.append(newImg);
+                    newGrid.append(newDiv);
+
+                    if (comingSoon[x] === true) {
+                        const addButton = document.createElement('button');
+                        addButton.id = "btnAddToWatchlist";
+                        addButton.className = "btnAddToWatchlist";
+                        addButton.innerHTML = "&#10133 Add to Watchlist";
+                                              
+                        newGrid.append(addButton);
+                    }
+                    else {
+                        const newLabel = document.createElement('label');
+                        newLabel.className = "lblComingSoon";
+                        newLabel.innerHTML = "Coming Soon!";
+
+                        newGrid.append(newLabel);
+                    }
+                }              
+
+                document.getElementById("moviesGrid").append(newGrid);               
+            }      
+        },
+        addMovieToWatchlist : function(e) {
+            console.log("Attempting to Add Movie to Watchlist");
+            console.log(e.path[1].id);
         }
     },
     created : function() {
         this.createMovies();
+        this.createMoviesGrid();
+    },
+    mounted() {
+        window.addEventListener('click', (e) => {
+            if (e.path[0].id === "btnAddToWatchlist") {
+                this.addMovieToWatchlist(e)
+            }
+        })
     }
 })
-
+/*
+export default {
+    data() {
+        return {
+            displayMovie : []
+        }
+    },
+    computed : {
+        moviesDisplay : function() {
+            return this.displayMovie;
+        }
+    }
+}
+*/
 /*
 //Prevent User to Return to Home Page Once They Have Signed Out
 function preventBack() {
