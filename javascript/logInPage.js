925e154ac09df55aed937f2249c7425d3bbc212f
Martin Louw <louw.martin05@gmail.com>
2022-08-08 10:28:56 +0200

    Log In Page Comments
    Added a default log in user, known as Admin.
    Added comments to different code sections.

--- javascript/logInPage.js
+++ javascript/logInPage.js
@@ -1,9 +1,23 @@
-console.log("Log In Page");
+//Create Default Sign In Information for an Admin
+let logInInformation = localStorage.getItem("users");
 
-//Remove Signed In User When User Uses Back Button
+if (!logInInformation) {
+    //Admin Sign In Information
+    let admin = { userName: "Admin" , userPassword : "Password" };
+    let adminArray = [];
+    adminArray.push(admin);
+    let adminString = JSON.stringify(adminArray);
+    let adminJson = JSON.parse(adminString);
+    localStorage.setItem("users", JSON.stringify(adminJson));
+}
+else {
+    //Sign In Information for Users Already Exist
+}
+
+//Remove Signed In User when the User Uses the Back Button
 localStorage.removeItem("currentUser");
 
-//Control Variables
+//Variables
 let signInSubmitBtn = document.getElementById("btnSignInSubmit");
 let signInSubmitBtnClass = document.getElementsByClassName("btnSignInSubmit");
 
@@ -28,27 +42,25 @@ window.onload = function() {
 }
 
 //Sign In
+//Event Listener for when a User Clicks the Submit Button to Sign In
 signInSubmitBtn.addEventListener('click', (e) => {
     let users = localStorage.getItem("users");
 
     if (users == null) {
-        console.log("No Registered Users Found");       
+        //No Registered Users Found  
         alert("Username or Password Incorrect.  Please Try Again.");
     }
     else if (users !== null) {
-        console.log("Registered Users Found");
-
+        //Registered Users Found
         e.preventDefault();
         signIn(users);        
     }
     else {
-        console.log("Something Went Wrong While Attempting To Retrieve Registered Users");
+        //Something went Wrong while Attempting to Retrieve Registered Users
     }
 })
 
 function signIn(users) {
-    console.log("Attempting To Sign In User");
-
     let signInUserName = userNameInput.value;
     let signInUserPassword = userPasswordInput.value;
 
@@ -62,7 +74,7 @@ function signIn(users) {
     let registeredObjString;
     let successObj;
     
-    //Cycle Through Registered Users For A Matching Entry
+    //Cycle Through Registered Users for a Matching Entry
     for (i = 0; i < usersLength; i++) {
         let registeredUserName = usersArray[i].userName;
         let registeredUserPassword = usersArray[i].userPassword;
@@ -71,26 +83,29 @@ function signIn(users) {
         registeredObjString = JSON.stringify(registeredObj);        
 
         if (signInObjString == registeredObjString) {
-            console.log("Matching Entry Found");
+            //Matching Entry Found
             successObj = JSON.parse(registeredObjString);
             signInArray.push(true);
         }
         else {
-            signInArray.push(false);
-            console.log("Not A Matching Entry");
+            //Not A Matching Entry
+            signInArray.push(false);            
         }
     }
 
-    //Check If There Was A Matching Entry
-    if (signInArray.includes(true)) {
-        
+    //Check if There was a Matching Entry
+    if (signInArray.includes(true)) { 
+        //Matching User Found
+        //Sign the User In       
         signInSuccess(successObj);
     }
     else {
+        //No Matching User Found
         alert("Username or Password Incorrect.  Please Try Again.");
     }
 }
 
+//Sign In was Successful
 function signInSuccess(user) {
     let signedInUser = user.userName;
     localStorage.setItem("currentUser", signedInUser);
@@ -99,8 +114,7 @@ function signInSuccess(user) {
 
 //Register User
 registerUserSubmitBtn.addEventListener('click', (e) => {
-    console.log("Attempting to Register a New User");
-
+    //Attempting to Register a New User
     let users = localStorage.getItem("users");
 
     let newUserName = userNameInput.value;
@@ -116,9 +130,9 @@ registerUserSubmitBtn.addEventListener('click', (e) => {
         let myJson = JSON.parse(jsonArray);
         let jsonData = JSON.stringify(myJson);
 
+        //Check if Sign In Information for other Users Already Exist
         if (users == null) {
-            console.log("No Registered Users Found");
-
+            //No Registered Users Found
             localStorage.setItem("users", jsonData);
             alert("New User Successfully Registered.  Please Attempt to Sign In.");
 
@@ -126,8 +140,7 @@ registerUserSubmitBtn.addEventListener('click', (e) => {
             hideRegisterBtn();
         }
         else if (users !== null) {
-            console.log("Registered Users Found");
-
+            //Registered Users Found
             e.preventDefault();
             registerUser(users, myJson);    
         }
@@ -140,25 +153,32 @@ registerUserSubmitBtn.addEventListener('click', (e) => {
     }
 })
 
+//Register User
 function registerUser(users, data) {
-    console.log("Register User Function")
     let usersArray = JSON.parse(users);
     let usersLength = usersArray.length;
     let duplicateArray = [];
     
+    //Check if Username Already Exists
     for (i = 0; i < usersLength; i++) {
         if (usersArray[i].userName == data[0].userName) {
+            //Matching Username Found
             duplicateArray.push(true);            
         }
         else {
+            //Usernames Do Not Match
             duplicateArray.push(false);
         }
     }
 
+    //Username Final Validation
     if (duplicateArray.includes(true)) {
+        //Duplicate Username was Found
         alert("Username Already Exists.  Please Choose a Different Username.");
     }
     else {
+        //No Duplicate Username Found
+        //Register New User's Information
         let newUsersArray = usersArray.concat(data);
         let newUsersData = JSON.stringify(newUsersArray);
         localStorage.setItem("users", newUsersData);
@@ -181,7 +201,7 @@ registerUserLabel.addEventListener('click', (e) => {
     userPasswordInput.value = '';
 })
 
-//Return To Sign In
+//Return to Sign In Form
 signInLabel.addEventListener('click', (e) => {
     hideRegisterBtn();
 })
@@ -199,7 +219,7 @@ function hideRegisterBtn() {
     userPasswordInput.value = '';
 }
 
-//Change Display Property
+//Change Label Display
 function changeDisplay(tab, display) {
     let i;
     let tabLength = tab.length;
